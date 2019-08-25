906f2d0d2dd5b135eaa56f3ffcb9d63175b0f9e6
strongSoda <strongsoda2@gmail.com>
2019-08-25 13:29:16 +0530

    refactor

--- driver.py
+++ driver.py
@@ -1,17 +1,17 @@
 """test for lsearch"""
-# from dsa.search.lsearch import search
+# from gopy.search.lsearch import search
 
 # l = [1,2,3,4,5,6,7,8,9]
 
 # print(search(8,l))
 
 """test for bsearch"""
-# from dsa.search.bsearch import search
+# from gopy.search.bsearch import search
 
 # l = [1,2,3,4,5,6,7,8,9]
 
 # print(search(1000000000,l))
 
 """test for bsearch"""
-# from pygorithms.sorting.bubble import sort
+# from gopy.sorting.bubble import sort
 # print(sort([1,2,3,4,5,6,7]))
