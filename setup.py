ddd65a2b66ff47af4b99be1f4375d483f51d5438
strongSoda <strongsoda2@gmail.com>
2019-08-25 13:18:24 +0530

    config

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Imran Khan',                   # Type in your name
   author_email = 'strongsoda2@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/strongSoda/data-structures-and-algorithms',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/strongSoda/data-structures-and-algorithms/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/strongSoda/data-structures-and-algorithms/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['data structures', 'algorithms', 'python'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
