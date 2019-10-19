aa9e01280276c6d76de5be7c8a89845299f50971
strongSoda <strongsoda2@gmail.com>
2019-10-19 21:52:29 +0530

    docs

--- gopy/sorting/bogo.py
+++ gopy/sorting/bogo.py
@@ -4,26 +4,21 @@ monkey sort is a particularly ineffective algorithm based on generate and test
 paradigm. The algorithm successively generates permutations of its input until 
 it finds one that is sorted.
 
-For example, if bogosort is used to sort a deck of cards, it would consist of 
-checking if the deck were in order, and if it were not, one would throw the 
-deck into the air, pick the cards up at random, and repeat the process until 
-the deck is sorted.
-
-### Complexity
-
-- Worst case time complexity: O((N+1)!)
-- Average case time complexity: O((N+1)!)
-- Best case time complexity: O(N)
-- Space complexity: O(1)
-
 """
 import random
  
 def bogosort(l):
     while not in_order(l):
-        random.shuffle(l)
+        random.shuffle(l) # throw all elements
     return l
  
+"""
+For example, if bogosort is used to sort a deck of cards, it would consist of 
+checking if the deck were in order, and if it were not, one would throw the 
+deck into the air, pick the cards up at random, and repeat the process until 
+the deck is sorted.
+"""
+
 def in_order(l):
     if not l:
         return True
@@ -33,6 +28,17 @@ def in_order(l):
             return False
         last = x
     return True
+
+
+"""
+### Complexity
+
+- Worst case time complexity: O((N+1)!)
+- Average case time complexity: O((N+1)!)
+- Best case time complexity: O(N)
+- Space complexity: O(1)
+
+"""
 
 def sort(array):
     return bogosort(array)
